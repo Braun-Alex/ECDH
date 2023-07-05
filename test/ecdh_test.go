b86076a12c5df0112ce1d735d119d8b94311b8a4
Braun_Alex <lamanasakasa@gmail.com>
2023-07-05 16:31:05 +0300

    Implementation of elliptic-curve Diffie-Hellman protocol has been tested

--- test/ecdh_test.go
+++ test/ecdh_test.go
@@ -1,1 +1,34 @@
 package test
+
+import (
+	"ECDH/pkg/ecdh"
+	"testing"
+)
+
+func TestRandomnessOfPrivateKeys(t *testing.T) {
+	privateKeyOfAlice, _ := ecdh.GenerateKeypair()
+	privateKeyOfBob, _ := ecdh.GenerateKeypair()
+	if privateKeyOfAlice.Cmp(privateKeyOfBob) == 0 {
+		t.Error("Private keys are not randomly generated")
+	}
+}
+
+func TestDiffieHellmanIncorrectUsage(t *testing.T) {
+	privateKeyOfAlice, publicKeyOfAlice := ecdh.GenerateKeypair()
+	privateKeyOfBob, publicKeyOfBob := ecdh.GenerateKeypair()
+	secretOfAlice := ecdh.GenerateSharedSecret(privateKeyOfAlice, publicKeyOfAlice)
+	secretOfBob := ecdh.GenerateSharedSecret(privateKeyOfBob, publicKeyOfBob)
+	if secretOfAlice.Cmp(secretOfBob) == 0 {
+		t.Error("Incorrect usage of Diffie-Hellman protocol must cause different shared secrets")
+	}
+}
+
+func TestDiffieHellmanCorrectUsage(t *testing.T) {
+	privateKeyOfAlice, publicKeyOfAlice := ecdh.GenerateKeypair()
+	privateKeyOfBob, publicKeyOfBob := ecdh.GenerateKeypair()
+	secretOfAlice := ecdh.GenerateSharedSecret(privateKeyOfAlice, publicKeyOfBob)
+	secretOfBob := ecdh.GenerateSharedSecret(privateKeyOfBob, publicKeyOfAlice)
+	if secretOfAlice.Cmp(secretOfBob) != 0 {
+		t.Error("Elliptic-curve Diffie-Hellman protocol has not been properly implemented")
+	}
+}
